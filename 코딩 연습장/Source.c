def9bd20a44e56b89d47e51585cc6193e59b72c4
ParkSeyang <ghlwkd14@naver.com>
2024-11-25 21:40:20 +0900

    (2024.11.25) 배열

---
+++ 코딩 연습장/Source.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+#pragma region Hello World 출력하기
+	// #include<stdio.h>
+
+	// int main()
+	// {
+	// printf("Hello World");
+	// }
+
+#pragma endregion
+
+#pragma region 
+
+#pragma endregion
+
+	
+}
