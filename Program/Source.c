0511b4a1df10d507706593dfa0fbf2cb18042571
ParkSeyang <ghlwkd14@naver.com>
2024-12-11 17:54:59 +0900

    (2024.12.11) 복습

--- Program/Source.c
+++ Program/Source.c
@@ -3,6 +3,18 @@
 
 int main()
 {
+	char arr[3][7] = { "가나다","Noxus","포인터" };
+	char(*data)[7];
+	
+	data = arr;
+
+	int i;
+
+	for (i = 0; i < 3; i++)
+	{
+		printf("%s\n", *(data + i));
+	}
+
 
 	return 0;
 }
